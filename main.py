fff3f467f12c52587db5b62de12f49d97ae3dbe7
Jhon Michael Molina <jmmmolinathebest@gmail.com>
2023-12-04 17:21:04 +0800

    Add routes

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+@app.route("/create")
+def create():
+    return render_template('create.html')
+
+
+@app.route("/take")
+def take():
+    return render_template('take.html')
+
 
 
 if __name__ == "__main__":
